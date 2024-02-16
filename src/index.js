b6dafe96eacd5b47675f502845ca3e1d830e6f01
pavan kumar <vpavankumar2105@gmail.com>
2024-02-17 01:54:14 +0530

    contact-added

--- src/index.js
+++ src/index.js
@@ -10,3 +10,18 @@ document.querySelectorAll(".nav-btn").forEach(n => n.addEventListener("click", (
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+
+function openResume(resumeUrl) {
+    // Open resume in a new tab
+    window.open(resumeUrl, '_blank');
+
+    // Download resume as PDF
+    var link = document.createElement('a');
+    link.href = resumeUrl;
+    link.download = 'resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
